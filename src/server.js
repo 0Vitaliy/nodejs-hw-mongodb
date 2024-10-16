a40da8be516d647c0b8c4adcac261f621c51c567
Vitalic <0makovskyi@gmail.com>
2024-10-16 11:40:16 +0300

    correction

--- src/server.js
+++ src/server.js
@@ -27,12 +27,6 @@ export const setupServer = () => {
     });
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     console.log(contacts);
@@ -59,6 +53,12 @@ export const setupServer = () => {
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
     });
   });
 
